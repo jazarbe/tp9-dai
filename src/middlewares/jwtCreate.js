1949e9580788f2e7bd0e47021c73542fb330a6c3
Android Studio <devandroid@ort>
2026-07-02 09:00:56 -0300

    empezamos jwt

---
+++ src/middlewares/jwtCreate.js
@@ -0,0 +1,41 @@
+// Ejemplo del campus
+// import jwt from 'jsonwebtoken';
+
+// Datos para incluir en el token (generalmente datos de usuario)
+// const payload = {
+//     id: 123,
+//     username: 'willywonka'
+// }
+// const secretKey = 'ClaveSecreta2000$'; // Clave secreta para firmar el token
+
+// Opciones para la firma del token (opcional)
+// const options = {
+//     expiresIn: '1h', // Expirará en 1 hora. 60, '7d', '4h', etc.
+//     issuer : 'mi_organizacion'
+// }
+
+// Generar el token de acceso
+// const token = jwt.sign (payload, secretKey, options);
+// console.log(token);
+
+
+// Ejemplo desencriptar
+// import jwt from 'jsonwebtoken';
+
+// Datos para incluir en el token (generalmente datos de usuario)
+// const payload = {
+//     id: 123,
+//     username: 'willywonka'
+// };
+// const secretKey = 'ClaveSecreta2000$'; // Clave secreta para firmar el token
+
+// Opciones para la firma del token (opcional)
+// const options = {
+//     expiresIn: '1h', // Expirará en 1 hora. 60, '7d', '4h', etc.
+//     issuer : 'mi_organizacion'
+// }
+
+// Generar el token de acceso
+// const token = jwt.sign (payload, secretKey, options);
+// console.log(token);
+
